a974ebc0ba1f26f2067089427dbb531f079009b7
Justin Kelly <justin@kelly.org.au>
2021-07-27 12:06:32 +1000

    Auto commit

--- do_extract.sh
+++ do_extract.sh
@@ -7,7 +7,7 @@ while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
   METHA_DIR=$oar_dir metha-sync $p
-done < oai_provider_list.txt
+done < provider_oai_list.txt
 
 git commit -am 'Auto commit'
 git push
