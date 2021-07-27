98809cd43cecabbee032d65d7675eb43ddc9ab3b
Justin Kelly <justin@kelly.org.au>
2021-07-27 12:13:27 +1000

    Auto commit

--- do_extract.sh
+++ do_extract.sh
@@ -3,16 +3,20 @@
 #Set your Metha extracts directory
 oai_dir="~/.cache/metha"
 
+printf 'Git pull \n'
 git pull
 
+printf 'Metha-sync \n'
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
   METHA_DIR=$oar_dir metha-sync $p
 done < provider_oai_list.txt
 
-./get_provder_info.sh
-./get_provder_xml.sh
+printf 'Run provider scripts \n'
+./get_provider_info.sh
+./get_provider_xml.sh
 
+printf 'Git commit and push \n'
 git commit -am 'Auto commit'
 git push
